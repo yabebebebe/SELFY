803787637d5dc1801baba0ed462379737b7490a3
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-29 02:00:02 +0900

    トップページの表示（仮）

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+  def idnex
+    
+  end
+
+end
--- db/migrate/20210728164816_devise_create_users.rb
+++ db/migrate/20210728164816_devise_create_users.rb
@@ -4,8 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.date   :birthday,           null: false
+      t.string :phone,              null: false
 
       ## Recoverable
       t.string   :reset_password_token

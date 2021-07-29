c0af5741ce48ec2cb2ec6fffe941511fccc6463e
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-30 00:06:13 +0900

    Active Storageの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :birthday, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:image, :nickname, :name, :birthday, :phone])
   end
 
 end

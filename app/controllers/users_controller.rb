c912aeeb221cac994ccce6a4cb9830288afee3e2
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-07-30 21:06:12 +0900

    物理削除による退会機能の実装（新機能）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
 
-  def idnex
+  def index
     
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
 end

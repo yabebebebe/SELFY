506df54f5ab58651a1587fdc0c6f3b70ff47f274
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-08-09 17:07:58 +0900

    エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :nickname
-    validates :name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）入力のみ可能です" }
     validates :birthday
-    validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid. Input only number" }
+    validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: "は半角数字（ハイフンは含まない）入力のみ可能です" }
   end
 
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角（英字・数字）の混合入力のみ可能です" }
 
 end

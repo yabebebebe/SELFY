1c48dc67590e9ac911cd12e901f6db6cb4b7fab4
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-08-09 21:23:56 +0900

    Basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_selfy
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def basic_selfy
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_SELFY_USER"] && password == ENV["BASIC_SELFY_PASSWORD"]
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:image, :nickname, :name, :birthday, :phone])

2e8d2b28e5ceed02d779b3b76df18f2419e0113a
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-08-10 19:52:37 +0900

    テーブルの修正

--- db/migrate/20210803104105_create_messages.rb
+++ db/migrate/20210803104105_create_messages.rb
@@ -1,8 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.string     :comment, null: false
-      t.references :user,    null: false, foreign_key: true
+      t.string     :comment,    null: false
+      t.references :user,       null: false, foreign_key: true
+      t.integer    :partner_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_08_05_145941) do
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "comment", null: false
     t.bigint "user_id", null: false
+    t.integer "partner_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_messages_on_user_id"

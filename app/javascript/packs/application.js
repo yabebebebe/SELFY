274834309df9e517a858c3e5ba3b1efcffdc2c67
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-08-11 21:49:55 +0900

    その他修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('../preview')
-require("@rails/jquery").start()
-require("@rails/jquery_ujs").start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

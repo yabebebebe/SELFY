59d6935936c0443029f800a4c0a3796d97a34b06
yabebebebe <86275209+yabebebebe@users.noreply.github.com>
2021-08-25 23:52:53 +0900

    既存テストコードの修正（ビュー以外）

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe Message, type: :model do
 
   describe '褒めちぎりコンテンツ新規投稿' do
     
-    context 'メッセージ送信が可能な時' do
+    context '新規投稿が可能な時' do
 
-      it '入力値が適切に存在する時' do
+      it '全ての項目の入力値が適切に存在する時' do
         expect(@message).to be_valid
       end
       
     end
 
-    context 'メッセージ送信が不可能な時' do
+    context '新規投稿が不可能な時' do
 
       it 'ユーザー情報が無い時' do
         @message.user = nil
@@ -23,10 +23,20 @@ RSpec.describe Message, type: :model do
         expect(@message.errors.full_messages).to include("ユーザーを入力してください")
       end
         
-      it 'commentが空の時' do
-        @message.comment = ''
-        @message.valid?
-        expect(@message.errors.full_messages).to include("メッセージを入力してください")
+      context '各項目の入力値が空の時' do
+        
+        it 'titleが空の時' do
+          @message.title = ''
+          @message.valid?
+          expect(@message.errors.full_messages).to include("タイトルを入力してください")
+        end
+
+        it 'textが空の時' do
+          @message.text = ''
+          @message.valid?
+          expect(@message.errors.full_messages).to include("内容を入力してください")         
+        end
+
       end
       
     end
